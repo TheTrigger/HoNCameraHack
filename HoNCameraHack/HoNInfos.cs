9f04bf9c693eb353eb32f62d6704f54990c44abe
TheTrigger <f.fabrizi91@gmail.com>
2019-01-16 14:25:43 +0100

    first commit

---
+++ HoNCameraHack/HoNInfos.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace HoNCameraHack
+{
+	public class HoNInfos
+	{
+		private string[] HoNPossiblePaths { get; } = new string[]
+		{
+			Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\Heroes of Newerth\game\game_shared.dll",
+			Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\Heroes of Newerth\game\game_shared.dll",
+		};
+
+
+		byte[] find_sequence = { 0x00, 0xE0, 0xF6, 0x44 }; // 1975 default
+		public float FindValue
+		{
+			get
+			{
+				return BitConverter.ToSingle(find_sequence, 0);
+			}
+			set
+			{
+				find_sequence = BitConverter.GetBytes(value);
+			}
+		}
+
+
+		byte[] patch_sequence = { 0x00, 0x80, 0x09, 0x45 }; // 2200
+
+		/// <summary>
+		/// https://gregstoll.com/~gregstoll/floattohex/
+		/// </summary>
+		public float PatchValue
+		{
+			get
+			{
+				return BitConverter.ToSingle(patch_sequence, 0);
+			}
+			set
+			{
+				patch_sequence = BitConverter.GetBytes(value);
+			}
+		}
+
+
+		private string _dllpath;
+		public string DllPath
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(_dllpath))
+					_dllpath = HoNPossiblePaths.Where(p => File.Exists(p)).First();
+				return _dllpath;
+			}
+		}
+
+
+		private Version _version;
+		public Version Version
+		{
+			get
+			{
+				try
+				{
+					if (_version == null)
+					{
+						var manifest = Directory.GetParent(Path.GetDirectoryName(DllPath)) + @"\manifest.xml";
+						using (var stream = new StreamReader(manifest))
+						{
+							using (var reader = XmlReader.Create(stream))
+							{
+								if (reader.MoveToContent() == XmlNodeType.Element && reader.Name == "manifest")
+								{
+									var value = reader.GetAttribute("version");
+									var version = Version.Parse(value);
+
+									_version = version;
+								}
+								else
+								{
+									throw new FileLoadException("Unable to get version");
+								}
+							}
+						}
+					}
+
+					return _version;
+				}
+				catch (Exception)
+				{
+					throw;
+				}
+			}
+		}
+
+
+
+		/// <summary>
+		/// Path game_shared.dll
+		/// </summary>
+		public List<int> FindOffset()
+		{
+			var addresses = new List<int>();
+
+			// the fastest way
+			var game_shared = File.ReadAllBytes(DllPath);
+			for (int i = 0; i < game_shared.Length - find_sequence.Length; i++)
+			{
+				int k = default;
+				while (k != find_sequence.Length && find_sequence[k] == game_shared[i + k++]) ;
+
+				if (k == 4)
+				{
+					addresses.Add(i);
+				}
+			}
+
+			return addresses;
+		}
+
+
+		public void Backup()
+		{
+			File.Copy(DllPath, $"{DllPath}.bak.{DateTime.Now.ToString("yyyyMMdd_HHmm")}", true);
+		}
+
+
+		public bool Patch(int offset)
+		{
+			try
+			{
+				this.Backup();
+
+				using (var stream = new FileStream(DllPath, FileMode.Open))
+				{
+					stream.Seek(offset, SeekOrigin.Begin);
+
+					stream.Write(patch_sequence, 0, patch_sequence.Length);
+
+					return true;
+				}
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
+}
---
+++ HoNCameraHack/Program.cs
@@ -0,0 +1,102 @@
+﻿using EasyConsole;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace HoNCameraHack
+{
+	class Program
+	{
+		public static bool IsAdministrator => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+		public static string Version => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+
+
+		static void Main(string[] args)
+		{
+			Console.Title = $"{nameof(HoNCameraHack)} - {Version}";
+			var honinfo = new HoNInfos();
+
+			if (!string.IsNullOrEmpty(honinfo.DllPath))
+			{
+				Console.WriteLine($"DLL Path: {honinfo.DllPath}");
+				Console.WriteLine($"HoN Version: {honinfo.Version}\n");
+
+				if (!Program.IsAdministrator)
+					Output.WriteLine(ConsoleColor.DarkRed, "You should run this program as Administrator or it may fail\n");
+
+				try
+				{
+					Console.WriteLine($"Default values: Find => {honinfo.FindValue} / Patch =>  {honinfo.PatchValue} ");
+
+					var addresses = honinfo.FindOffset();
+
+					switch (addresses.Count())
+					{
+						case 0:
+							{
+								Output.WriteLine(ConsoleColor.Red, @"/!\ No offset found :( already patched?");
+								break;
+							}
+						case 1:
+							{
+								var offset = addresses.First();
+								Console.WriteLine($"Offset found! {offset:X}");
+
+								if (honinfo.Patch(offset))
+								{
+									Output.WriteLine(ConsoleColor.Green, "PATCHED!");
+								}
+								else
+								{
+									Output.WriteLine(ConsoleColor.Red, "Unable to patch :(");
+								}
+
+								break;
+							}
+
+						default:
+							{
+								var menu = new EasyConsole.Menu();
+
+								Output.WriteLine(ConsoleColor.Yellow, @"/!\ Multiple offsets found");
+								Console.WriteLine($"recommended to patch the first");
+								for (int i = 0; i < addresses.Count(); i++)
+								{
+									var address = addresses[i];
+									menu.Add($"{address:X}", () =>
+									{
+										if (honinfo.Patch(address))
+										{
+											Output.WriteLine(ConsoleColor.Green, "PATCHED!");
+										}
+										else
+										{
+											Output.WriteLine(ConsoleColor.Red, "Unable to patch :(");
+										}
+
+									});
+								}
+
+								menu.Display();
+								break;
+							}
+					}
+				}
+				catch (Exception ex)
+				{
+					Output.WriteLine(ConsoleColor.DarkRed, $"{ex.GetType()} => {ex.Message}" + Environment.NewLine);
+				}
+			}
+			else
+			{
+				Output.WriteLine(ConsoleColor.Red, "HoN not found :(");
+			}
+
+
+			Output.WriteLine(ConsoleColor.Gray, "Press any key to close...");
+			Console.ReadLine();
+		}
+	}
+}
